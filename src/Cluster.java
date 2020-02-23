6d42a179d34a9bd18be2a827612c41e9f6c34fb7
xyronic15 <xyronb@gmail.com>
2020-02-23 16:29:44 -0500

    started cluster class

--- src/Cluster.java
+++ src/Cluster.java
@@ -1,4 +1,13 @@
+import java.util.*;
+
 public class Cluster {
 
-    
+    int id;
+    Points2D centroid;
+    List<Integer> pointsIds;
+
+    public Cluster(int id, Points2D centroid){
+        this.id = id;
+        
+    }
 }
