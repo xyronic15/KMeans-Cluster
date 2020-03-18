fe055dc880833bf922cccbcb58e8cdd569de8b87
t-abhi-patel <abhi.patel@telus.com>
2020-03-18 14:01:26 -0400

    updated visualize.py

--- visualize/README.md
+++ visualize/README.md
@@ -1,7 +1,11 @@
 ## This file shows how to visualize the data
 
+Note: Requires python3 + anaconda/miniconda
+
+0) Open anaconda prompt (Windows)
+
 1) `cd KMeans-Cluster/visualize`
 
 2) `pip3 install -r requirements.txt `
 
-3) `python3 visualize.py` 
+3) `python3 visualize.py`
--- visualize/visualize.py
+++ visualize/visualize.py
@@ -11,22 +11,43 @@ try:
 except:
     print("cannot open input/points.txt")
 
+
 min_K = 2
 max_K = 4
+
+fig, axs = plt.subplots(max_K-min_K+1, figsize=(15,15))
+fig.suptitle('K-Means Test', fontsize=16)
+
+count = 0
+
 for i in range(min_K, max_K+1):
 
     try:
         centroids = np.genfromtxt(f'../output/centroids_K={i}.txt', delimiter = ' ')
         c_x = np.array(centroids[:,0])
         c_y = np.array(centroids[:,1])
-        
-    except:
-        print("cannot open output/centroids.txt")
 
-    try:
-       
-        plt.scatter(x, y, marker = 'o', label='data points')	
-        plt.scatter(c_x, c_y, marker = 'x', label='centroids')
-        plt.show()
+        axs[count].plot(c_x, c_y, 'o', x, y, 'x')
+        axs[count].set_title(f'K={i}')
+        count+=1
+
     except:
-        print("plotting failed")
+        print(f'cannot open output/centroids_K={i}.txt')
+
+plt.show()
+
+try:
+
+    centroids = np.genfromtxt('../output/centroids_K=4.txt', delimiter = ' ')
+    c_x = np.array(centroids[:,0])
+    c_y = np.array(centroids[:,1])
+
+    vor = Voronoi(centroids)
+    voronoi_plot_2d(vor)
+
+    plt.scatter(x, y, marker = 'o', label='data points')
+    plt.scatter(c_x, c_y, marker = 'o', label='centroids')
+    plt.show()
+
+except:
+    print("plotting failed")
